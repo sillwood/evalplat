d433a49ba1870c19e0b87b71660250fba3659970
Amy Kirasack <amykirasack@Amys-MacBook-Pro.local>
2022-10-13 15:54:03 +0100

    style: tailwind ui setup and dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+          colors: {
+            primary: '#017235'
+          },
+          fontFamily: {
+            sans: ['Inter var'],
+          },
+        },
     },
     plugins: [],
 };
