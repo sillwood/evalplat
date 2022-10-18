e72334c54f061049c42ec7a1271aee7aede6bc5d
Amy Kirasack <amykirasack@Amys-MacBook-Pro.local>
2022-10-18 11:04:17 +0100

    style: login and register

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,6 @@ export const Navbar = (props: Props) => {
                         </div>
                         <div className="hidden md:block">
                             <div className="ml-10 flex items-baseline space-x-4">
-                                {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-green-700 hover:text-white" --> */}
                                 <a
                                     href="#"
                                     className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-green-700 hover:text-white"
@@ -71,7 +70,7 @@ export const Navbar = (props: Props) => {
                         {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-green-700 hover:text-white" --> */}
                         <a
                             href="#"
-                            className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+                            className="block rounded-md px-3 py-2 text-base font-medium  text-gray-300 hover:bg-green-700 hover:text-white"
                             aria-current="page"
                         >
                             Dashboard
